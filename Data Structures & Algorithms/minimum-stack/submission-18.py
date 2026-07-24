c17eccb0ba058ed5aa71d1f13642c4fa9b41edd9
Long (Jack) Thien Ngo <108920424+longngo2312@users.noreply.github.com>
2026-07-24 16:24:15 -0500

    Add: minimum-stack - submission-18

---
+++ Data Structures & Algorithms/minimum-stack/submission-18.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        if not self.minStack: 
+            self.minStack.append(val)
+        elif val <= self.minStack[-1]:
+            self.minStack.append(val)
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        value = self.stack.pop()
+        if value == self.minStack[-1]:
+            self.minStack.pop()
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
